b06d16beaaed4ca8fbf6ff2b1b679adf062b9e31
fanshicomic <fanshicomic@gmail.com>
2016-03-01 01:18:14 +0100

    remove unnecessary js links for some files

--- Scripts/Controllers/orderBeerController.js
+++ Scripts/Controllers/orderBeerController.js
@@ -1,3 +1,11 @@
+$("#showoosbeers").click(function() {
+	if ($("#showoosbeers:checked").length < 1) {
+		$('.danger[style="display: table-row;"]').attr({style: 'display: none;'});
+	} else {
+		$('.danger[style="display: none;"]').attr({style: 'display: table-row;'});
+	}
+});
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
