d72b32428a13a9a1975857e3bd66c24451689184
fanshicomic <fanshicomic@gmail.com>
2016-03-14 01:15:16 +0100

    okay basic setup for internationalization done! basic function is there already, will translate them tmr

---
+++ Scripts/Controllers/languageTranslator.js
@@ -0,0 +1,29 @@
+$(function() {
+	$('.flag').click(function() {
+		get_language(this);
+		change_language();
+	});
+});
+
+function get_language(flag) {
+	if ($(flag).hasClass('uk')) {
+		window.localStorage.setItem('lang', 'uk');
+	} else {
+		window.localStorage.setItem('lang', 'sv');
+	}
+}
+
+function change_language() {
+    var language = window.localStorage.getItem('lang');
+    $.ajax({
+        url: '../../language.xml',
+        success: function(xml) {
+            $(xml).find('translation').each(function(){
+                var id = $(this).attr('id');
+                var text = $(this).find(language).text();
+                $("." + id).html(text);
+            });
+        }
+    });
+}
+
